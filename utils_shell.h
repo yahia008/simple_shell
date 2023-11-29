5dfee7cb30ab174b0cc014d2b7ccf121d52efbad
yahia008 <yahyatijjani99@gmail.com>
2023-11-29 11:08:23 -0800

    hesder

---
+++ utils_shell.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADE
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+
+#endif
